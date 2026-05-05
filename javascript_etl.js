e0d2f43e3e47d555636902937aac04687d68315c
enx2026 <jordanjcq@gmail.com>
2026-05-05 13:05:46 +0200

    Update javascript_etl.js

--- javascript_etl.js
+++ javascript_etl.js
@@ -1,6 +1,4 @@
-	  window.addEventListener('DOMContentLoaded', () => {
-
-new Swiper('.photo-swiper', { loop: true, allowTouchMove: false, speed: 4000, spaceBetween: 20, slidesPerView: 1.15, breakpoints: { 576: { slidesPerView: 1.8, spaceBetween: 20 }, 768: { slidesPerView: 2.4, spaceBetween: 24 }, 992: { slidesPerView: 3, spaceBetween: 28 }, 1200: { slidesPerView: 3.2, spaceBetween: 28 } }, autoplay: { delay: 0, disableOnInteraction: false } });
+ new Swiper('.photo-swiper', { loop: true, allowTouchMove: false, speed: 4000, spaceBetween: 20, slidesPerView: 1.15, breakpoints: { 576: { slidesPerView: 1.8, spaceBetween: 20 }, 768: { slidesPerView: 2.4, spaceBetween: 24 }, 992: { slidesPerView: 3, spaceBetween: 28 }, 1200: { slidesPerView: 3.2, spaceBetween: 28 } }, autoplay: { delay: 0, disableOnInteraction: false } });
     new Swiper('.sponsor-swiper', { loop: true, freeMode: true, allowTouchMove: false, speed: 4000, spaceBetween: 40, slidesPerView: 3, breakpoints: { 768: { slidesPerView: 4, spaceBetween: 50 }, 992: { slidesPerView: 5, spaceBetween: 60 }, 1200: { slidesPerView: 6, spaceBetween: 70 } }, autoplay: { delay: 0, disableOnInteraction: false } });
     // Countdown to 2 Dec 2026
     (function () {
@@ -18,7 +16,7 @@ new Swiper('.photo-swiper', { loop: true, allowTouchMove: false, speed: 4000, sp
     })();
     // Active nav on scroll
     (function () {
-      var sections = ['top', 'programme', 'speakers', 'venue', 'partners', 'become-partner'];
+      var sections = ['top', 'venue'];
       var links = document.querySelectorAll('.nav-links a');
       window.addEventListener('scroll', function () {
         var pos = window.scrollY + 80;
@@ -33,4 +31,3 @@ new Swiper('.photo-swiper', { loop: true, allowTouchMove: false, speed: 4000, sp
         }
       });
     })();
-     });
